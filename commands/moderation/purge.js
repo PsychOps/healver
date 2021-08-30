44942716b22e7b3968c8da65108d779bc8fbcc95
joshua <joshuaslui0203@gmail.com>
2021-08-30 23:01:26 +0200

    update purge.js

---
+++ commands/moderation/purge.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'purge',
+
+    description: 'Purge the chat',
+
+
+    async execute(message, args, client) {
+        await message.reply(':warning: | This command is still in the works!');
+    },
+};
