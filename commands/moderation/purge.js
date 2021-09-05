bff4c88c278672121433688c25416effeedad9e7
joshua <joshuaslui0203@gmail.com>
2021-09-05 21:30:24 +0200

    Some purge stuff

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -3,8 +3,18 @@ module.exports = {
 
     description: 'Purge the chat',
 
+    args: true,
+
+    usage: '<messagecount>',
 
     async execute(message, args, client) {
-        await message.reply(':warning: | This command is still in the works!');
+        //await message.reply(':warning: | This command is still in the works!');
+        args++;
+        const channel = message.channel
+        let test = await channel.messages.fetch({ limit: 5})
+        console.log(test)
+        await message.channel.bulkDelete(args.toString())
+        //await message.channel.bulkDelete(args[1].join(''))
+        await message.channel.send(`Success!`);
     },
 };
