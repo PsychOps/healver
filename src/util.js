7b58a462e2f1f9c88895aff1ce7f4b0f871af4df
joshua <joshuaslui0203@gmail.com>
2021-09-06 21:39:44 +0200

    add invite link

--- src/util.js
+++ src/util.js
@@ -29,7 +29,7 @@ util.color ={
 
 util.links ={
     support: 'https://discord.gg/QMhvHuGPMx',
-    invite: 'notarickroll.com'
+    invite: 'https://discord.com/api/oauth2/authorize?client_id=884515940967006309&permissions=103079603265&scope=bot'
 }
 
 util.escapeFormatting = (string) => {
