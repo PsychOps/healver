d8db8458a92e346cdb317c5fa86a4784dfe600f2
joshua <joshuaslui0203@gmail.com>
2021-09-06 21:14:32 +0200

    Comment out mysql-example.js

--- src/commands/moderation/mysql-example.js
+++ src/commands/moderation/mysql-example.js
@@ -1,18 +1,18 @@
-module.exports = {
-    name: 'mysql-example',
+//module.exports = {
+//    name: 'mysql-example',
 
-    description: 'Query some data.',
+//    description: 'Query some data.',
 
-    usage: '',
+//    usage: '',
 
-    cooldown: 3,
+//    cooldown: 3,
 
-    async execute(message, args, client, database) {
+//    async execute(message, args, client, database) {
         // // insert something
         //await database.query("INSERT INTO example (content) VALUES (?)", ['example-value']);
         // select something
-        const data = await database.query("SELECT * FROM example");
+        //const data = await database.query("SELECT * FROM example");
         //use that data
-        message.reply(data[0][0]?.content);
-    },
-};
+        //message.reply(data[0][0]?.content);
+    //},
+//};
