d999a2e6ac7938d39ec81da873956c3f2955a306
joshua <joshuaslui0203@gmail.com>
2021-09-11 10:59:52 +0200

    Uptime!!

--- src/commands/utility/botinfo.js
+++ src/commands/utility/botinfo.js
@@ -13,10 +13,13 @@ module.exports = {
         embed.setColor(color.blue)
         embed.setDescription(`${client.user.username} is a multifunctional bot developed and maintained by ${(await client.users.fetch('843866750131109909')).tag} in co-operation with ${(await client.users.fetch('282825580141871104')).tag} and ${(await client.users.fetch('104933285506908160')).tag} to provide users with modules such as utility, moderation and mental support. ${client.user.username} is a product by [Modraxis Development](https://github.com/Modraxis). The code can be found [here on github](https://github.com/Modraxis/Healver) and is licensed under the GPL 3.0 license. For inquiries, contact **modraxisorg@gmail.com**.`)
 
+        const uptime = (Date.now() / 1000 - client.uptime / 1000).toFixed(0);
+
         let stats = '';
         stats += `**Users:** ${client.users.cache.size} \n`
         stats += `**Guilds:** ${client.guilds.cache.size} \n`
-        stats += `**Created:** <t:${Math.round(client.user.createdAt.getTime() / 1000)}:R>`
+        stats += `**Created:** <t:${Math.round(client.user.createdAt.getTime() / 1000)}:R> \n`
+        stats += `**Up since:** <t:${uptime}:R>`
 
         embed.addFields(
             {name: 'statistics', value: stats, inline: false}
